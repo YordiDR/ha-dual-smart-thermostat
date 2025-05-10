fb1fed0a8b0faf993bbbb90fef142d6148306a9d
Yordi De Rijcke <yordi.derijcke@live.be>
2025-05-10 10:11:17 +0200

    cooler_controller.py: Take always_on into account for cooler start

--- custom_components/dual_smart_thermostat/hvac_controller/cooler_controller.py
+++ custom_components/dual_smart_thermostat/hvac_controller/cooler_controller.py
@@ -81,3 +81,40 @@ class CoolerHvacController(GenericHvacController):
             self._hvac_action_reason = strategy.goal_not_reached_reason()
         else:
             _LOGGER.debug("No case matched when - keep device on")
+
+    # override
+    async def async_control_device_when_off(
+        self,
+        strategy: HvacEnvStrategy,
+        any_opening_open: bool,
+        time=None,
+    ) -> None:
+        """Check if we need to turn heating on or off when the heater is off."""
+        _LOGGER.info("%s Controlling hvac while off", self.__class__.__name__)
+        _LOGGER.debug("above_env_attr: %s", strategy.hvac_goal_reached)
+        _LOGGER.debug("below_env_attr: %s", strategy.hvac_goal_not_reached)
+        _LOGGER.debug("any_opening_open: %s", any_opening_open)
+        _LOGGER.debug("is_active: %s", True)
+        _LOGGER.debug("time: %s", time)
+        _LOGGER.debug("always_on: %s", self._always_on)
+
+        if (self._always_on or strategy.hvac_goal_not_reached) and not any_opening_open:
+            _LOGGER.info(
+                "Turning on entity (from inactive) due to hvac goal is not reached %s",
+                self.entity_id,
+            )
+            await self.async_turn_on_callback()
+            self._hvac_action_reason = strategy.goal_not_reached_reason()
+        elif time is not None or any_opening_open:
+            # The time argument is passed only in keep-alive case
+            _LOGGER.info("Keep-alive - Turning off entity %s", self.entity_id)
+            await self.async_turn_off_callback()
+
+            if any_opening_open:
+                self._hvac_action_reason = HVACActionReason.OPENING
+        else:
+            _LOGGER.debug("No case matched when - keeping device off")
+            if strategy.hvac_goal_reached:
+                self._hvac_action_reason = strategy.goal_reached_reason()
+            else:
+                self._hvac_action_reason = strategy.goal_not_reached_reason()
